d3a556aea4352e97f57fb13a248ef6dc8b3c4c6d
Esley <esleynathan@hotmail.com>
2022-12-28 00:57:44 -0300

    Sistema de Login - Controller Cadastro

---
+++ controller.py
@@ -0,0 +1,49 @@
+from model import Pessoa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import hashlib
+
+def retorna_session():
+    CONN = "sqlite:///projeto2.db"
+    engine = create_engine(CONN, echo=True)
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+class ControllerCadastro:
+    @classmethod
+    def verifica_dados(cls, nome, email, senha):
+        if len(nome) > 50 or len(nome) <3:
+            print("Digite um nome valido")
+            return 2
+        if len(email) > 200:
+            print("Digite um nome valido")
+            return 3
+        if len(senha) > 100 or len(senha) <6:
+            print("Digite um nome valido")
+            return 4
+
+        return 1
+
+    @classmethod
+    def cadastrar(cls, nome, email, senha):
+        session = retorna_session()
+        usuario = session.query(Pessoa).filter(Pessoa.email == email).all()
+
+        if len(usuario) > 0:
+            return 5
+
+        dados_verificados = cls.verifica_dados(nome, email, senha)
+
+        if dados_verificados != 1:
+            return cls.verifica_dados
+
+        try:
+            senha = hashlib.sha256(senha.encode()).hexdigest()
+            p1 = Pessoa(nome=nome, email=email, senha=senha)
+            session.add(p1)
+            session.commit()
+            return 1
+        except:
+            return 3
+
+ControllerCadastro.cadastrar('Ésley','esleyOutro@email.com', 'nego12345')
